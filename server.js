400819387137805b01e67e6ec546ed951e05dd90
rhenandias <diasrhenan@gmail.com>
2022-02-11 21:36:23 -0300

    Criação do boilerplate do projeto e teste de conexão com o banco de dados Postgres

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const sequelize = require("./src/services/sequelize");
+
+// Configuração das Variáveis de Ambiente
+dotenv.config({ path: ".env" });
+
+// Configuração do Express
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "1gb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "1gb",
+  })
+);
+
+app.use("/api", require("./src/routes"));
+
+app.get("/connection", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error.message);
+  }
+
+  res.status(200).send({
+    message: "Biblioteca de Bolso - API",
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Biblioteca de Bolso - API",
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).send({
+    message: "Biblioteca de Bolso - API",
+  });
+});
+
+// Iniciando Servidor
+let port = process.env.PORT;
+if (port == "" || port == null) {
+  port = 4005;
+}
+app.listen(port);
