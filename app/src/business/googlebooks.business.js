ee4e869f6a6f895157660bd1b78ec3188d206c50
rhenandias <diasrhenan@gmail.com>
2022-03-20 23:44:44 -0300

    Integração com a Google Books API

---
+++ app/src/business/googlebooks.business.js
@@ -0,0 +1,59 @@
+const { Success } = require("../modules/codes");
+const { ok } = require("../modules/http");
+const { googleBooksAPI } = require("../modules/googlebooks");
+
+module.exports = {
+  async search(qstring) {
+    let books = [];
+
+    await googleBooksAPI
+      .get("/volumes", {
+        params: {
+          key: process.env.GOOGLE_BOOKS_API,
+          q: qstring,
+        },
+      })
+      .then((response) => {
+        const results = response.data.items;
+
+        for (const book of results) {
+          // Identificação dos ISBN 10 e 13
+          let ibsn_10 = "";
+          let isbn_13 = "";
+
+          for (const isbn of book.volumeInfo["industryIdentifiers"]) {
+            if (isbn["identifier"].length == 10) ibsn_10 = isbn["identifier"];
+            else if (isbn["identifier"].length == 13) isbn_13 = isbn["identifier"];
+          }
+
+          // Identificação da capa do livro
+          let thumbnail = "";
+
+          if (book.volumeInfo.hasOwnProperty("imageLinks")) {
+            thumbnail = book.volumeInfo["imageLinks"]["thumbnail"];
+          }
+
+          books.push({
+            ibsn_10: ibsn_10,
+            isbn_13: isbn_13,
+            title: book.volumeInfo["title"],
+            author: book.volumeInfo["authors"][0],
+            publisher: book.volumeInfo["publisher"],
+            publishedDate: book.volumeInfo["publishedDate"],
+            description: book.volumeInfo["description"],
+            thumbnail: thumbnail,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("Erro:", error.name, error.message);
+      });
+
+    return ok({
+      code: Success,
+      response: {
+        books: books,
+      },
+    });
+  },
+};
--- app/src/routes.js
+++ app/src/routes.js
@@ -5,16 +5,24 @@ const UserController = require("./controllers/user.controller");
 const BookController = require("./controllers/book.controller");
 const AuthController = require("./controllers/auth.controller");
 const PlaygroundController = require("./controllers/playground.controller");
+const GoogleBooksController = require("./controllers/googlebooks.controller");
 
+// Rotas de Playground
 routes.get("/play/date", PlaygroundController.date);
 
+// Rotas de Autenticação
 routes.post("/auth/login", AuthController.login);
 routes.get("/auth/verify", AuthController.verifyAccount);
 routes.post("/auth/refresh", AuthController.refreshToken);
 
+// Rotas de Usuário
 routes.post("/user", UserController.create);
 routes.delete("/user", UserController.delete);
 
+// Rotas de Livros
 routes.get("/book/list", BookController.list);
+
+// Rotas do Google Books
+routes.get("/googlebooks/", GoogleBooksController.search);
 
 module.exports = routes;
