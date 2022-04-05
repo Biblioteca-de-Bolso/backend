eb32ce095a2636ed2ecdbf7de190f0ea2b20271c
rhenandias <diasrhenan@gmail.com>
2022-04-05 14:18:19 -0300

    Alterações nos schemas e novas rotas

--- app/src/controllers/book.controller.js
+++ app/src/controllers/book.controller.js
@@ -6,6 +6,7 @@ const ISBNValidator = require("../validators/book/isbn.rules");
 const PublisherValidator = require("../validators/book/publisher.rules");
 const DescriptionValidator = require("../validators/book/description.rules");
 const ThumbnailValidator = require("../validators/book/thumbnail.rules");
+const BookIdValidator = require("../validators/book/id.rules");
 
 const validation = require("../modules/validation");
 
@@ -47,6 +48,28 @@ module.exports = {
     } catch (error) {
       return next(error);
     }
+  },
+
+  async read(req, res, next) {
+    // Aquisição do token
+    const { token } = req;
+
+    // Aquisição e validação dos parâmetros
+    const { id } = req.params;
+
+    const rules = [[id, BookIdValidator]];
+
+    const validationResult = validation.run(rules);
+
+    if (validationResult.status === "error") {
+      return res.status(400).json(validationResult);
+    }
+
+    // Token validado, prosseguir com a requisição
+    const response = await BookBusiness.read(token);
+
+    // Retornar com resultado da operação
+    return res.status(response.statusCode).json(response.body);
   },
 
   async list(req, res, next) {
