91b4ce7f22c19a02523df6efdcd7d003dab2e592
rhenandias <diasrhenan@gmail.com>
2022-08-02 20:35:59 -0300

    Adicionando seeds para um novo usuário

--- app/prisma/seeds/db.seed.js
+++ app/prisma/seeds/db.seed.js
@@ -7,6 +7,7 @@ module.exports = async () => {
 
   const users = [
     // A conta do Allan Turing é utilizada para o teste de ativação de conta de usuário
+    // Livros desse usuário: 1 e 2
     {
       name: "Allan Turing",
       email: "allan@email.com",
@@ -37,6 +38,7 @@ module.exports = async () => {
       },
     },
     // A conta da Ada é utilizada para o teste de leitura de usuário e de login
+    // Livros desse usuário: 3 e 4
     {
       name: "Ada Lovelace",
       email: "ada@email.com",
@@ -69,6 +71,7 @@ module.exports = async () => {
       },
     },
     // A conta do Von Neumann deve permencer inativa para falhar no teste de login
+    // Livros desse usuário: 5 e 6
     {
       name: "Von Neumann",
       email: "vonneumann@email.com",
@@ -99,6 +102,7 @@ module.exports = async () => {
       },
     },
     // A conta do Dennis é utilizada para o teste de remoção de usuário
+    // Livros desse usuário: 7 e 8
     {
       name: "Dennis Ritchie",
       email: "dennisritchie@email.com",
@@ -123,6 +127,37 @@ module.exports = async () => {
               isbn13: "9786588431214",
               publisher: "Editora do Livro de Compiladores",
               description: "Descrição do livro de Compiladores",
+            },
+          ],
+        },
+      },
+    },
+    // A conta do Charles Babbage é utilizada nos testes de listagem de livros
+    // Livros desse usuário: 9 e 10
+    {
+      name: "Charles Babbage",
+      email: "charlesbabbage@email.com",
+      password: crypto.createHash("md5").update("charlesbabbage").digest("hex"),
+      active: true,
+      activationCode: "1234567812345678",
+      books: {
+        createMany: {
+          data: [
+            {
+              title: "Matemática Discreta",
+              author: "Autor do Matemática Discreta",
+              isbn10: "8550811483",
+              isbn13: "9788550811482",
+              publisher: "Editora do Matemática Discreta",
+              description: "Descrição do Matemática Discreta",
+            },
+            {
+              title: "Máquinas Digitais",
+              author: "Autor do Máquinas Digitais",
+              isbn10: "658843121X",
+              isbn13: "9786588431214",
+              publisher: "Editora do Máquinas Digitais",
+              description: "Descrição do Matemática Discreta",
             },
           ],
         },
