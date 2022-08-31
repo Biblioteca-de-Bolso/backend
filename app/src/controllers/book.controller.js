950510d1fe824d5ef623f11059ce08aa97fa4aea
Rhenan <diasrhenan@gmail.com>
2022-08-31 14:41:45 -0300

    Corrigindo falha com envio de bookId como string na rota de alteração de capa de livro

--- app/src/controllers/book.controller.js
+++ app/src/controllers/book.controller.js
@@ -229,7 +229,11 @@ module.exports = {
 
       const userId = parseInt(token["id"], 10);
 
-      const bookId = parseInt(req.body.bookId, 10);
+      let bookId = "";
+
+      if (req.body.bookId) {
+        bookId = parseInt(req.body.bookId.replace(/\D/g, ""), 10);
+      }
 
       const thumbnailFile = req.file;
 
@@ -255,7 +259,11 @@ module.exports = {
 
       const userId = parseInt(token["id"], 10);
 
-      const bookId = parseInt(req.body.bookId, 10);
+      let bookId = "";
+
+      if (req.body.bookId) {
+        bookId = parseInt(req.body.bookId.replace(/\D/g, ""), 10);
+      }
 
       const rules = [[bookId, BookIdValidator, { required: true, allowEmpty: false }]];
 
